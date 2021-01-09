e6d1f9b9dab853c37c38870a3c919a7973816f08
slavaider <slavaider1@gmail.com>
2021-01-09 17:15:33 +0500

    fix navigation

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -10,27 +10,30 @@ import StarShipDetails from "../starship-details/starship-details";
 import PersonDetails from "../person-details";
 import PlanetDetails from "../planet-details/planet-details";
 
+const repo_name = 'star_db'
 
 function App() {
-        return (
-            <div className='container-fluid'>
-                <Header/>
-                <RandomPlanet/>
-                <div className="row mb2">
+    return (
+        <div className='container-fluid'>
+            <Header/>
+            <RandomPlanet/>
+            <div className="row mb2">
+                <Switch>
+                    <Route
+                        path={[`/${repo_name}/starships/:id`, `/${repo_name}/people/:id`, `/${repo_name}/planets/:id`]}
+                        component={ItemList}/>
+                    <Redirect from={'*'} to={`/${repo_name}/people/1`}/>
+                </Switch>
+                <div className="col-md-6">
                     <Switch>
-                        <Route path={["/starships/:id", "/people/:id", "/planets/:id"]} component={ItemList}/>
-                        <Redirect from={'*'} to={`/people/1`}/>
+                        <Route path={`/${repo_name}/starships/:id`} component={StarShipDetails}/>
+                        <Route path={`/${repo_name}/people/:id`} component={PersonDetails}/>
+                        <Route path={`/${repo_name}/planets/:id`} component={PlanetDetails}/>
                     </Switch>
-                    <div className="col-md-6">
-                    <Switch>
-                        <Route path='/starships/:id' component={StarShipDetails}/>
-                        <Route path='/people/:id' component={PersonDetails}/>
-                        <Route path='/planets/:id' component={PlanetDetails}/>
-                    </Switch>
-                    </div>
                 </div>
             </div>
-        );
+        </div>
+    );
 }
 
 export default App;
